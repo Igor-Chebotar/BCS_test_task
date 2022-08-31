2905a4b3386e209016737e6d8a67369e30039b23
Igor-Chebotar <igorcebotar5@gmail.com>
2022-08-31 15:00:36 +0300

    Update url

--- manager/urls.py
+++ manager/urls.py
@@ -1,11 +1,10 @@
 from django.contrib import admin
 from django.urls import path
 
-from .views import TransactionsView, TransactionDescriptionView, hello
+from .views import TransactionsView, TransactionDescriptionView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', TransactionsView.as_view()),
     path('<int:pk>', TransactionDescriptionView.as_view()),
-    path('hello', hello, name='hello_name')
 ]
